69628389a62ca3df21675dda079b1e79575edc6e
EREMIN Artem <artem.eremin@raiffeisen.ru>
2023-01-20 18:47:21 +0300

    Delete instant converters

--- src/main/java/ru/eremin/ad/board/config/R2dbcConfiguration.java
+++ src/main/java/ru/eremin/ad/board/config/R2dbcConfiguration.java
@@ -1,28 +1,9 @@
 package ru.eremin.ad.board.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.r2dbc.config.EnableR2dbcAuditing;
-import org.springframework.data.r2dbc.convert.R2dbcCustomConversions;
-import ru.eremin.ad.board.config.converter.InstantReadingConverter;
-import ru.eremin.ad.board.config.converter.InstantWritingConverter;
-
-import java.util.List;
 
 @Configuration
 @EnableR2dbcAuditing
 public class R2dbcConfiguration {
-
-    /**
-     * Регистрация конвертеров для Instant, для сохранения в базу
-     */
-    @Bean
-    public R2dbcCustomConversions r2dbcCustomConversions() {
-        return new R2dbcCustomConversions(
-            List.of(
-                new InstantReadingConverter(),
-                new InstantWritingConverter()
-            )
-        );
-    }
 }
